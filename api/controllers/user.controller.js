280e8418321e7338182f197feb659874dae048b4
Pavan <pinnintipavan04@gmail.com>
2024-07-23 09:17:48 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,23 +7,40 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
-    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own account'));
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, 'You can only update your own account!'));
     try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password, 10)
-        }
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set:{
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar,
-            }
-        }, {new: true});
+      if (req.body.password) {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar,
+          },
+        },
+        { new: true }
+      );
+  
+      const { password, ...rest } = updatedUser._doc;
+  
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+};
 
-        const {password, ...rest} = updateUser._doc
-
-        res.status(200).json(rest);
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account'));
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.clearCookie('access_token');
+        res.status(200).json('User has been deleted');
     } catch (error) {
         next(error)
     }
