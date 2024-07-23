11c0b7bf53354058ef506ba6c5a2a757cba435f3
Pavan <pinnintipavan04@gmail.com>
2024-07-23 22:38:45 +0530

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux"
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
-import {deleteUserFailure, deleteUserStart, deleteUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess} from '../redux/user/userSlice'
+import {deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess} from '../redux/user/userSlice'
 import { useDispatch } from "react-redux";
 import { app } from '../firebase';
 
@@ -84,6 +84,21 @@ export default function Profile() {
         dispatch(deleteUserFailure(error.message));
       }
     }
+
+    const handleSignOut = async() => {
+      try {
+        dispatch(signOutUserStart())
+        const res = await fetch(`/api/auth/signout`);
+        const data = await res.json()
+        if(data.success === false){
+          dispatch(deleteUserFailure(data.message))
+          return;
+        }
+        dispatch(deleteUserSuccess(data))
+      } catch (error) {
+        dispatch(deleteUserFailure(data.message))
+      }
+    }
     return (
       <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -110,7 +125,7 @@ export default function Profile() {
         </form>
         <div className="flex justify-between mt-5">
           <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-          <span className="text-red-700 cursor-pointer">Sign out</span>
+          <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
         </div>
         <p className="text-red-700 mt-5">{error ? error : ''}</p>
         <p className="text-green-700 mt-5">{updateSuccess ? 'User is updated Successfully' : ''}</p>
